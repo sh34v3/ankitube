2af17c3dc56f36dd19d59698799c6619b0ea7a9c
hunt0x3r <buginformation@protonmail.ch>
2022-03-15 13:10:25 -0400

    ankitube.py, moving away from ui file structure

--- window.py
+++ window.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout, QWidget
+from PyQt5.QtWidgets import QMenuBar, QMenu, QAction, QApplication, QMainWindow, QLabel, QGridLayout, QWidget
 from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
 
@@ -13,12 +13,24 @@ class UI(QMainWindow):
         ui_path = os.path.join(dir_path, "ankitube.ui")
         uic.loadUi(ui_path, self)
 
-        img_path = os.path.join(dir_path, 'sandstone.jpg')
-        self.imLabel = self.findChild(QLabel, "label")
-        self.pixmap = QPixmap(img_path)
-        self.imLabel.setPixmap(self.pixmap)
+        self.file = self.findChild(QMenu, "menu_file")
+        self.help = self.file.addAction("Help")
+        self.file.triggered[QAction].connect(self.showDialog())
 
-        self.show()
+        self.exec()
+
+    def showDialog(self, q):
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setText("Ankitube is designed to allow download of YouTube video clips easily for \
+                        educational purposes. Simply copy the URL into the text box, enter the \
+                        clip start and end times. FFMpeg is a required dependency to allow .mp4 to \
+                        .webm conversion to ensure anki video player compatability.\n \n")
+        msgBox.setWindowTitle("Help")
+        msgBox.setStandardButtons(QMessageBox.Ok)
+        msgBox.buttonClicked.connect(self.msgButtonClick)
+
+        returnValue = msgBox.exec()
         
 
 if __name__ == '__main__':
